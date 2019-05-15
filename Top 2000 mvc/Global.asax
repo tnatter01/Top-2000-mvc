9584fbfe804c91c1b0c4df9eabdd5a220ef0e3e8
Twan Natter <twan180401@gmail.com>
2019-05-15 13:31:49 +0200

    Add project files.

---
+++ Top 2000 mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Top_2000_mvc.MvcApplication" Language="C#" %>
---
+++ Top 2000 mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Top_2000_mvc.Startup))]
+namespace Top_2000_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
