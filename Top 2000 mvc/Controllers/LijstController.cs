8e85fe215e8eaa8d28586be004805f884237e794
Twan Natter <twan180401@gmail.com>
2019-06-14 16:04:51 +0200

    controllers toegevoegd

---
+++ Top 2000 mvc/Controllers/LijstController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using Top_2000_mvc.Models;
+using System.Web.Mvc;
+
+namespace Top_2000_mvc.Controllers
+{
+    public class LijstController : Controller
+    {
+        private LijstEntities2 db = new LijstEntities2();
+
+
+        // GET: Lijst
+        public ActionResult Index()
+        {
+            var liedjes = from Lijst in db.Lijst
+                          where
+                            Lijst.top2000jaar == 2018
+                          orderby
+                            Lijst.positie
+                          select new
+                          {
+                              Lijst.positie,
+                              Lijst.Song.titel,
+                              Lijst.Song.Artiest.naam,
+                              Lijst.Song.jaar
+                          };
+
+            return View(liedjes.ToList());
+        }
+
+        // GET: Lijst/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Lijst/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lijst/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Lijst/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Lijst/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Lijst/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Lijst/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Top 2000 mvc/Controllers/SongsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Top_2000_mvc.Models;
+
+namespace Top_2000_mvc.Controllers
+{
+    public class SongsController : Controller
+    {
+        private LijstEntities2 db = new LijstEntities2();
+
+        // GET: Songs
+        public ActionResult Index()
+        {
+            var song = db.Song.Include(s => s.Artiest);
+            return View(song.ToList());
+        }
+
+        // GET: Songs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Song song = db.Song.Find(id);
+            if (song == null)
+            {
+                return HttpNotFound();
+            }
+            return View(song);
+        }
+
+        // GET: Songs/Create
+        public ActionResult Create()
+        {
+            ViewBag.artiestid = new SelectList(db.Artiest, "artiestid", "naam");
+            return View();
+        }
+
+        // POST: Songs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "songid,artiestid,titel,jaar")] Song song)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Song.Add(song);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.artiestid = new SelectList(db.Artiest, "artiestid", "naam", song.artiestid);
+            return View(song);
+        }
+
+        // GET: Songs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Song song = db.Song.Find(id);
+            if (song == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.artiestid = new SelectList(db.Artiest, "artiestid", "naam", song.artiestid);
+            return View(song);
+        }
+
+        // POST: Songs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "songid,artiestid,titel,jaar")] Song song)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(song).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.artiestid = new SelectList(db.Artiest, "artiestid", "naam", song.artiestid);
+            return View(song);
+        }
+
+        // GET: Songs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Song song = db.Song.Find(id);
+            if (song == null)
+            {
+                return HttpNotFound();
+            }
+            return View(song);
+        }
+
+        // POST: Songs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Song song = db.Song.Find(id);
+            db.Song.Remove(song);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
